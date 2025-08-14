d8f7e653f603e486e51b35c80223e9b7d33da9d5
MX <notanactualemail@gmail.com>
2025-08-14 14:02:48 +0200

    sending get in touch form to httpbin

--- src/js/script.js
+++ src/js/script.js
@@ -121,7 +121,21 @@ try {
           .querySelector("#question")
           .parentElement.parentElement.querySelector(".checkbox-error-message"),
       }
-    );
+    )
+    .onSuccess((event) => {
+      const form = event.currentTarget;
+      const formData = new FormData(form);
+
+      fetch("https://httpbin.org/post", {
+        method: "POST",
+        body: formData,
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          console.log("Success", data);
+          form.reset();
+        });
+    });
 } catch (e) {}
 
 //footer form validation
